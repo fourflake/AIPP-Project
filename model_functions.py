b791ca2070d2449018f596b2a8f8e4f34f7d8ed6
Joeri Jessen <joeri_jessen@hotmail.com>
2020-04-19 13:16:57 +0200

    **Changed model_functions.py to work locally
    - Removed method (active_session) for training on Udacity server, which causes a connection error on local system

--- model_functions.py
+++ model_functions.py
@@ -1,6 +1,6 @@
 """
 Image Classifier Project.
-Written by Joeri Jessen 
+Written by Joeri Jessen
 Date: 15/04/2020
 
 model_functions.py contains all necessary functions to build and train a network on a dataset,
@@ -10,7 +10,6 @@ import torch
 from torch import nn, optim
 import torch.nn.functional as F
 from torchvision import datasets, transforms, models
-from workspace_utils import active_session
 import utility_functions as utility
 
 def build(n_classes, arch='vgg16',hidden_units=400,gpu=False):
@@ -30,17 +29,16 @@ def build(n_classes, arch='vgg16',hidden_units=400,gpu=False):
         input_units = model.classifier[0].in_features
     else:
         input_units = model.classifier[1].in_features
-        
+
     model.classifier = nn.Sequential(nn.Linear(input_units,hidden_units),
                                 nn.ReLU(),
                                 nn.Dropout(0.3),
                                 nn.Linear(hidden_units,hidden_units),
-                                nn.ReLU(),     
                                 nn.ReLU(),
                                 nn.Linear(hidden_units,n_classes),
                                 nn.LogSoftmax(dim=1))
     model.to(device)
-    
+
     return model
 
 
@@ -49,61 +47,60 @@ def train(model,dataloaders,print_every = 4,learning_rate=0.001,gpu=False,epochs
     Prints out training loss, validation loss, and validation accuracy as the network trains
     '''
     device = torch.device("cuda" if (gpu==True and torch.cuda.is_available()) else "cpu")
-    
+
     criterion = nn.NLLLoss()
     optimizer = optim.Adam(model.classifier.parameters(),learning_rate)
 
-    steps= 0 
+    steps= 0
     model.epochs = epochs
 
-    with active_session():
-        for e in range(epochs):
-            train_loss = 0
-            for images, labels in dataloaders['train']:
-                steps += 1
-                images, labels = images.to(device), labels.to(device)
+    for e in range(epochs):
+        train_loss = 0
+        for images, labels in dataloaders['train']:
+            steps += 1
+            images, labels = images.to(device), labels.to(device)
 
-                logps = model(images)
-                loss = criterion(logps,labels)
-                ps = torch.exp(logps)
+            logps = model(images)
+            loss = criterion(logps,labels)
+            ps = torch.exp(logps)
 
-                optimizer.zero_grad()
-                loss.backward()
-                optimizer.step()
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
 
-                train_loss += loss
-                
-                if steps % print_every == 0:
-                    accuracy = 0
-                    valid_loss = 0
-                    model.eval()
+            train_loss += loss
 
-                    with torch.no_grad():
-                        for images,labels in dataloaders['valid']:
+            if steps % print_every == 0:
+                accuracy = 0
+                valid_loss = 0
+                model.eval()
 
-                            images, labels = images.to(device), labels.to(device)
+                with torch.no_grad():
+                    for images,labels in dataloaders['valid']:
 
-                            logps = model(images)
-                            loss = criterion(logps,labels)
-                            ps = torch.exp(logps)
+                        images, labels = images.to(device), labels.to(device)
 
-                            top_p,top_cl = ps.topk(1,dim=1)
+                        logps = model(images)
+                        loss = criterion(logps,labels)
+                        ps = torch.exp(logps)
 
-                            equals = top_cl == labels.view(*top_cl.shape)
-                            accuracy += torch.mean(equals.type(torch.FloatTensor))
-                            valid_loss += loss
+                        top_p,top_cl = ps.topk(1,dim=1)
 
-                        print(f"Epoch: {e+1}/{epochs} .. "
-                              f"Train Loss: {train_loss/len(dataloaders['train']):.3} .. "
-                              f"Test Loss: {valid_loss/len(dataloaders['valid']):.3} .. " 
-                              f"Accuracy: {accuracy/len(dataloaders['valid']):.3}")
-                    model.train()     
-            print("Epoch completed")  
-        print("Training model completed.")   
+                        equals = top_cl == labels.view(*top_cl.shape)
+                        accuracy += torch.mean(equals.type(torch.FloatTensor))
+                        valid_loss += loss
+
+                    print(f"Epoch: {e+1}/{epochs} .. "
+                          f"Train Loss: {train_loss/len(dataloaders['train']):.3} .. "
+                          f"Test Loss: {valid_loss/len(dataloaders['valid']):.3} .. "
+                          f"Accuracy: {accuracy/len(dataloaders['valid']):.3}")
+                model.train()
+        print("Epoch completed")
+    print("Training model completed.")
     model.optimizer = optimizer
-    return model 
-    
-    
+    return model
+
+
 def predict(image_path, model, top_k=1, gpu=False):
     ''' Predict the class (or classes) of an image using a trained deep learning model.
     '''
@@ -112,20 +109,16 @@ def predict(image_path, model, top_k=1, gpu=False):
 
     im = utility.process_image(image_path)
     im_in = im.view(1, 3, 224, 224).to(device)
-    
+
     with torch.no_grad():
         model.eval()
         logps = model(im_in)
         ps = torch.exp(logps)
 
     probs, idx_list = ps.topk(top_k,dim=1)
-    
+
     idx_to_class = {v: k for k, v in model.class_to_idx.items()}
     class_list =[]
     [class_list.append(idx_to_class[idx]) for idx in idx_list.cpu().numpy().tolist()[0]]
-    
+
     return probs.cpu().numpy()[0], class_list
-    
-
-
-
